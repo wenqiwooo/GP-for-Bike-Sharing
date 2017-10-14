fa5ae31888930968a3b85d2859f753764d5f987b
therealwenqi <therealwenqi@hotmail.com>
2017-10-14 14:55:51 +0800

    added map images and visualizer plot image method

--- visualizer.py
+++ visualizer.py
@@ -1,2 +1,14 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
 class Visualizer:
-  pass
+  @staticmethod
+  def plot_img(img_path):
+    img = mpimg.imread(img_path)
+    imgplot = plt.imshow(img)
+
+
+if __name__ == '__main__':
+  Visualizer.plot_img('./assets/sk_map.png')
+  plt.show()
